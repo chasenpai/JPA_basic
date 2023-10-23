da860731878f75cf2dd61c5d459e2764ea2492e0
junwvwv <wvwv0612@gmail.com>
2023-10-23 19:53:53 +0900

    엔티티 필드 매핑

--- ex1/src/main/java/hello/Member.java
+++ ex1/src/main/java/hello/Member.java
@@ -3,9 +3,8 @@ package hello;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "member")
@@ -16,7 +15,25 @@ public class Member {
     @Id
     private Long id;
 
+    @Column(name = "name", updatable = false, nullable = false)
     private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //enum 이름을 DB 에 저장 - ORDINAL 사용 X
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP) //LocalDate, LocalDateTime 사용 시 생략 가능
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob //BLOB, CLOB 타입 매핑
+    private String description;
+
+    @Transient //필드 매핑 & 저장 & 조회 X - 주로 메모리상에서 임시로 값 보관 시 사용용
+   private Integer temp;
 
     public Member() { //JPA 엔티티는 기본생성자가 필수 public or protected - 자바 리플렉션을 활용하여 동적으로 객체를 생성
     }
